7cd40482bde627fbdc5decd09a1cc10d0584021c
Skynse <34770859+Skynse@users.noreply.github.com>
2021-10-23 20:41:18 -0500

    formatted code

--- src/bufread.rs
+++ src/bufread.rs
@@ -1,6 +1,5 @@
-
-use std::io::prelude::*;
 use std::fs::File;
+use std::io::prelude::*;
 
 pub struct HexDump;
 
@@ -9,9 +8,7 @@ fn sanitize(hex: &str) -> String {
     if hex.trim().len() == 1 {
         temp.push_str(hex);
         return temp;
-    }
-
-    else {
+    } else {
         return hex.to_string();
     }
 }
@@ -27,60 +24,58 @@ fn sanitize_chars(c: String) -> String {
 
         if value > 32 && value < 128 {
             temp.push(chr);
+        } else {
+            temp.push('.');
         }
-            else {
-                temp.push('.');
-            }
-        }
+    }
     return temp;
-
 }
 
 impl HexDump {
-
     pub fn new() -> Self {
         HexDump {}
     }
-    pub fn read(&mut self, path: &str) -> Result<(), std::io::Error>{
+    pub fn read(&mut self, path: &str) -> Result<(), std::io::Error> {
         let mut output: String = String::new();
         let mut file = File::open(path)?;
         let mut offset: u32 = 0;
 
         loop {
             let mut temp_chunks = Vec::with_capacity(16); //load new byte chunk
-            let n = std::io::Write::by_ref(&mut file).take(16 as u64).read_to_end(&mut temp_chunks)?;
-            let mut chars = sanitize_chars(temp_chunks.iter().map(|x| *x as char).collect::<String>());
+            let n = std::io::Write::by_ref(&mut file)
+                .take(16 as u64)
+                .read_to_end(&mut temp_chunks)?;
+            let mut chars =
+                sanitize_chars(temp_chunks.iter().map(|x| *x as char).collect::<String>());
             let mut count = 0;
             output.push_str(format!("{:08x}: ", offset).as_str());
 
             if n == 0 {
                 break;
-            }
-            
-            else {
+            } else {
                 for byte in &temp_chunks {
-                    if count == 8 {output.push_str("| ")}
+                    if count == 8 {
+                        output.push_str("| ")
+                    }
                     output.push_str(sanitize(format!("{:X?} ", byte).as_str()).as_str());
-                    
+
                     count += 1;
                 }
 
-                if temp_chunks.len()%16 != 0 {
-                    output.push_str("   ".repeat(16-temp_chunks.len()).as_str()); 
+                if temp_chunks.len() % 16 != 0 {
+                    output.push_str("   ".repeat(16 - temp_chunks.len()).as_str());
                     output.push_str(chars.as_str());
-                }
-                else {
+                } else {
                     output.push_str(chars.as_str());
                 }
 
                 println!("{}", output);
-                
             }
             offset += 16;
             output.clear();
             chars.clear();
         }
-        
+
         Ok(())
     }
 }
@@ -91,7 +86,7 @@ mod tests {
     use std::{any::Any, fmt::Result, io::Bytes};
 
     #[test]
-    fn test_byte_struct()  {
+    fn test_byte_struct() {
         let test_struct = HexDump::new();
         assert_eq!(1, 1);
     }
